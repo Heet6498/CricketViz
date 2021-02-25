08fd8e2a44cff6a6f19a66505558dcbaee5189fe
HEET SONI <58238162+Heet6498@users.noreply.github.com>
2021-02-24 23:05:37 -0500

    Update viz.py

--- viz.py
+++ viz.py
@@ -22,7 +22,7 @@ if checkbox:
 #Histogram
 
 st.sidebar.subheader("Count_plot")
-new_df = data.loc[data['team'] == 'India']
+new_df = data.loc[data['team'] == 'India']                              # want to convert this filter as dropdown.
 #select_box = st.sidebar.selectbox(label='Teams', options=team)
 select_box1 = st.sidebar.selectbox(label='X axis', options=columns)
 select_box2 = st.sidebar.selectbox(label="Hue", options=columns)
